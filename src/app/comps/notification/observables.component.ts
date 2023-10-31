67938824e681945e77e1a28817660bde000a4b39
Sumita Gondaliya <sumitag.albiorix@gmail.com>
2023-10-31 12:39:22 +0530

    no message

--- src/app/comps/notification/observables.component.ts
+++
@@ -1,7 +0,0 @@
-
-import { fromEvent } from 'rxjs';
-const clicks = fromEvent(document, 'click');
-
-clicks.subscribe(event => {
-    // console.log('Mouse Clicked at:', event.clientX);
-});
--- src/app/comps/steps/steps.components.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-steps',
-  template: `<div>Steps</div>`,
-  styles: ['']
-})
-export class StepsComponent {}
